d26f38b96249ccd5a036c9f56c159255b2b4ff57
Hilmar X <x@hilmarx.com>
2020-11-14 13:17:12 +0100

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,88 @@
+// Buidler
+const { task, types } = require("hardhat/config");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+
+// Libraries
+const assert = require("assert");
+const { utils } = require("ethers");
+
+require("dotenv").config();
+
+// @dev Put this in .env
+const ALCHEMY_ID = process.env.ALCHEMY_ID;
+assert.ok(ALCHEMY_ID, "no Alchemy ID in process.env");
+
+// @dev fill this out
+const DEPLOYER = "YOUR ADDRESS"; //
+const DEPLOYER_PK_MAINNET = process.env.DEPLOYER_PK_MAINNET;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  // hardhat-deploy
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      mainnet: DEPLOYER,
+    },
+  },
+  networks: {
+    hardhat: {
+      // Standard config
+      // timeout: 150000,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_ID}`,
+        blockNumber: 11189230,
+      },
+      // Custom
+      GelatoCore: "0x1d681d76ce96E4d70a88A00EBbcfc1E47808d0b8",
+    },
+    mainnet: {
+      accounts: DEPLOYER_PK_MAINNET ? [DEPLOYER_PK_MAINNET] : [],
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_ID}`,
+      gasPrice: parseInt(utils.parseUnits("10", "gwei")),
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.10",
+        settings: {
+          optimizer: { enabled: process.env.DEBUG ? false : true },
+        },
+      },
+      {
+        version: "0.7.4",
+        settings: {
+          optimizer: { enabled: process.env.DEBUG ? false : true },
+        },
+      },
+    ],
+    // overrides: {
+    //   "contracts/vendor/DependenciesSix.sol": {
+    //     version: "0.6.10",
+    //     settings: {
+    //       optimizer: {enabled: process.env.DEBUG ? false : true},
+    //     },
+    //   },
+    //   "@gelatonetwork/core/contracts/gelato_core/GelatoCore.sol": {
+    //     version: "0.6.10",
+    //     settings: {
+    //       optimizer: {enabled: process.env.DEBUG ? false : true},
+    //     },
+    //   },
+    //   "@gelatonetwork/core/contracts/external/Ownable.sol": {
+    //     version: "0.6.10",
+    //     settings: {
+    //       optimizer: {enabled: process.env.DEBUG ? false : true},
+    //     },
+    //   },
+    // },
+  },
+};
