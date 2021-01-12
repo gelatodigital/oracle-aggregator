0a8c4577ce0e93416b9a654614814843275392dd
Hilmar X <x@hilmarx.com>
2021-01-12 12:42:43 +0100

    refactored Oracle Aggreagtor

--- contracts/interfaces/IOracle.sol
+++ contracts/interfaces/IOracle.sol
@@ -3,5 +3,6 @@ pragma solidity 0.6.10;
 
 interface IOracle {
     function latestAnswer() external view returns (uint256);
+
     function decimals() external view returns (uint256);
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,37 +1,45 @@
-const { getAggregatedOracles } = require('../test/helper');
-const { ethers, network } = require('hardhat');
+const { getAggregatedOracles } = require("../test/helper");
+const { ethers, network } = require("hardhat");
 
 async function main() {
-  if (network.name == 'mainnet') {
-    console.log('for safety mainnet deployment is disallowed by default\nsee scripts/deploy.js to allow')
-    return
+  if (network.name == "mainnet") {
+    console.log(
+      "for safety mainnet deployment is disallowed by default\nsee scripts/deploy.js to allow"
+    );
+    return;
   }
   const [deployer] = await ethers.getSigners();
-  
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
-  
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  const { tokensA, tokensB, oracles, stablecoins, decimals } = getAggregatedOracles();
-  const GelatoOracleAggregator = await ethers.getContractFactory("OracleAggregator", deployer);
+  const {
+    tokensA,
+    tokensB,
+    oracles,
+    stablecoins,
+    decimals,
+  } = getAggregatedOracles();
+  const GelatoOracleAggregator = await ethers.getContractFactory(
+    "OracleAggregator",
+    deployer
+  );
   const contract = await GelatoOracleAggregator.deploy(
     network.config.addresses.wethAddress,
     tokensA,
     tokensB,
     oracles,
     stablecoins,
-    decimals,
+    decimals
   );
 
   console.log("Contract address:", contract.address);
 }
-  
+
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
